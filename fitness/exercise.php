8c57f129eec46e3ae9496e3fb25d804893bdf04f
ghfig2 <ghfig@hotmail.com>
2015-10-16 03:45:15 +0000

    Formatting code

--- fitness/exercise.php
+++ fitness/exercise.php
@@ -1,25 +1,27 @@
 <?php
-   session_start();
-     $exercises = $_SESSION['fitrak'];
-     if($_POST){
-       if(isset($_GET['id'])){
-         $exercises[$_GET['id']] = $_POST;
-       }else{
-         $exercises[] = $_POST;
-       }
-       
-       $_SESSION['fitrak'] = $exercises;
-     }
-       
-     if(isset($_GET['edit'])){
-       $exercise = $exercises[$_GET['edit']];
-     } else if(isset($_GET['delete'])){
-         unset($exercises[$_GET['delete']]);
-         $_SESSION['fitrak'] = $exercises;
-     } else{
-       $exercise = array();
-     }
-   ?>
+
+session_start();
+$exercises = $_SESSION['fitrak'];
+
+if ($_POST) {
+    if (isset($_GET['id'])) {
+        $exercises[$_GET['id']] = $_POST;
+    } else {
+        $exercises[] = $_POST;
+    }
+    $_SESSION['fitrak'] = $exercises;
+}
+
+if (isset($_GET['edit'])) {
+    $exercise = $exercises[$_GET['edit']];
+} else if (isset($_GET['delete'])) {
+    unset($exercises[$_GET['delete']]);
+    $_SESSION['fitrak'] = $exercises;
+} else {
+    $exercise = array();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
    <head>
@@ -64,12 +66,10 @@
             <!--/.nav-collapse -->
          </div>
       </nav>
-      
       <div class="container">
          <br>
          <br>
          <br>
-
          <div class="row">
             <div class="col-md-5">
                <h3>What have you done?</h3>
@@ -77,10 +77,9 @@
                <p>
                   <button type="button" class="btn btn-primary btn-lg" id="aerobic">Aerobic</button> or 
                   <button type="button" class="btn btn-primary btn-lg" id="workout">Work Out</button>
-               </p>                
-                
+               </p>
                <form role="form" action="" method="POST" id="aerobic-form" style="display: none">
-                   <input type="hidden" name="type" value="Aerobic">
+                  <input type="hidden" name="type" value="Aerobic">
                   <div class="form-group" id="aerobic-exercises">
                      <label for="exercise">Select the exercise you did:</label>
                      <select class="form-control" name="exercise" id="exercise" required>
@@ -101,10 +100,9 @@
                   <div class="form-group">
                      <button type="submit" class="btn btn-success" id="submit">Save</button>
                   </div>
-               </form>                
-
+               </form>
                <form role="form" action="" method="POST" id="workout-form" style="display: none">
-                   <input type="hidden" name="type" value="Work Out">
+                  <input type="hidden" name="type" value="Work Out">
                   <div class="form-group" id="workout-exercises">
                      <label for="exercise">Select the exercise you did:</label>
                      <select class="form-control" name="exercise" id="exercise" required>
@@ -128,12 +126,9 @@
                   <div class="form-group">
                      <button type="submit" class="btn btn-success" id="submit">Save</button>
                   </div>
-            </form>
-            
+               </form>
             </div>
-            
             <div class="col-md-6">
-                
                <h2>Your exercises:</h2>
                <p>List of exercises you have done yet</p>
                <h4>Aerobic</h4>
@@ -148,27 +143,23 @@
                      </tr>
                   </thead>
                   <tbody>
-                  
-                  <?php 
-                  foreach($exercises as $id => $ex): 
-                  if ($ex['type'] == "Aerobic") { ?>
+                     <?php 
+                        foreach($exercises as $id => $ex): 
+                        if ($ex['type'] == "Aerobic") { ?>
                      <tr>
                         <td><?=$ex['exercise'];?></td>
                         <td><?=$ex['duration'];?></td>
                         <td><?=$ex['distance'];?></td>
                         <td>563</td>
                         <td>
-                        <a href="?edit=<?=$id;?>"><span class="glyphicon glyphicon-edit text-warning"></span></a>
-                        <a href="?delete=<?=$id;?>"><span class="glyphicon glyphicon-remove text-danger"></span></a>
+                           <a href="?edit=<?=$id;?>"><span class="glyphicon glyphicon-edit text-warning"></span></a>
+                           <a href="?delete=<?=$id;?>"><span class="glyphicon glyphicon-remove text-danger"></span></a>
                         </td>
                      </tr>
-
-               <?php }
-               endforeach; ?>
+                     <?php }
+                        endforeach; ?>
                   </tbody>
                </table>
-               
-               
                <h4>Work Out</h4>
                <table class="table table-hover">
                   <thead>
@@ -178,38 +169,33 @@
                         <th>Weight</th>
                         <th>Calories burned</th>
                         <th></th>
-                        </tr>
+                     </tr>
                   </thead>
                   <tbody>
-                  
-                  <?php 
-                  foreach($exercises as $id => $ex): 
-                  if ($ex['type'] == "Work Out") { ?>
+                     <?php 
+                        foreach($exercises as $id => $ex): 
+                        if ($ex['type'] == "Work Out") { ?>
                      <tr>
                         <td><?=$ex['exercise'];?></td>
                         <td><?=$ex['series'];?></td>
                         <td><?=$ex['weight'];?></td>
                         <td>563</td>
                         <td>
-                        <a href="?edit=<?=$id;?>"><span class="glyphicon glyphicon-edit text-warning"></span></a>
-                        <a href="?delete=<?=$id;?>"><span class="glyphicon glyphicon-remove text-danger"></span></a>
+                           <a href="?edit=<?=$id;?>"><span class="glyphicon glyphicon-edit text-warning"></span></a>
+                           <a href="?delete=<?=$id;?>"><span class="glyphicon glyphicon-remove text-danger"></span></a>
                         </td>
                      </tr>
-
-               <?php }
-               endforeach; ?>
+                     <?php }
+                        endforeach; ?>
                   </tbody>
                </table>
-               
                <hr>
-
-      </div>
-      
             </div>
-      <!-- Site footer -->
-      <footer class="footer text-center">
-         <p>&copy; FITrack 2015</p>
-      </footer>
+         </div>
+         <!-- Site footer -->
+         <footer class="footer text-center">
+            <p>&copy; FITrack 2015</p>
+         </footer>
       </div>
       <!-- /container -->
       <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
@@ -217,42 +203,39 @@
       <!-- Include all compiled plugins (below), or include individual files as needed -->
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
       <script>
-      
-      $(document).ready(function(){
-          var edtion = '<?php echo $_GET["edit"]; ?>';
-          if(edtion) {
-              var type = '<?php echo $exercise["type"]; ?>';
-              if (type == "Aerobic") {
-             $("#aerobic").prop("class","btn btn-primary btn-lg");
-             $("#workout").prop("class","btn btn-default btn-lg");
-             $("#workout").prop("disabled","disabled");
-             $("#aerobic-form").show();
-                  
-            } else if (type == "Work Out"){
-             $("#workout").prop("class","btn btn-primary btn-lg");
-             $("#aerobic").prop("class","btn btn-default btn-lg");
-             $("#aerobic").prop("disabled","disabled");
-             $("#workout-form").show();
-                
-            }
-          }
-      });
-
-
-      
-         $("#aerobic").click(function(){
-             $("#aerobic").prop("class","btn btn-primary btn-lg");
-             $("#workout").prop("class","btn btn-default btn-lg disabled");
-             $("#workout-form").hide();
-             $("#aerobic-form").show();
+         $(document).ready(function(){
+             var edtion = '<?php echo $_GET["edit"]; ?>';
+             if(edtion) {
+                 var type = '<?php echo $exercise["type"]; ?>';
+                 if (type == "Aerobic") {
+                $("#aerobic").prop("class","btn btn-primary btn-lg");
+                $("#workout").prop("class","btn btn-default btn-lg");
+                $("#workout").prop("disabled","disabled");
+                $("#aerobic-form").show();
+                     
+               } else if (type == "Work Out"){
+                $("#workout").prop("class","btn btn-primary btn-lg");
+                $("#aerobic").prop("class","btn btn-default btn-lg");
+                $("#aerobic").prop("disabled","disabled");
+                $("#workout-form").show();
+                   
+               }
+             }
          });
          
-         $("#workout").click(function(){
-             $("#workout").prop("class","btn btn-primary btn-lg");
-             $("#aerobic").prop("class","btn btn-default btn-lg disabled");
-             $("#aerobic-form").hide();
-             $("#workout-form").show();
-         });
+            $("#aerobic").click(function(){
+                $("#aerobic").prop("class","btn btn-primary btn-lg");
+                $("#workout").prop("class","btn btn-default btn-lg disabled");
+                $("#workout-form").hide();
+                $("#aerobic-form").show();
+            });
+            
+            $("#workout").click(function(){
+                $("#workout").prop("class","btn btn-primary btn-lg");
+                $("#aerobic").prop("class","btn btn-default btn-lg disabled");
+                $("#aerobic-form").hide();
+                $("#workout-form").show();
+            });
       </script>
    </body>
 </html>
--- fitness/index.php
+++ fitness/index.php
@@ -1,27 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Fitrack // Get fit!</title>
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+      <title>Fitrack // Get fit!</title>
+      <!-- Bootstrap -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-
-    <!-- Fixed navbar -->
+      <![endif]-->
+   </head>
+   <body>
+      <!-- Fixed navbar -->
       <nav class="navbar navbar-default navbar-fixed-top">
          <div class="container">
             <div class="navbar-header">
@@ -48,50 +43,43 @@
             <!--/.nav-collapse -->
          </div>
       </nav>
-
-    <div class="container">
-        <br>
-        <br>
-        <br>
-        <!-- Jumbotron -->
-        <div class="jumbotron">
+      <div class="container">
+         <br>
+         <br>
+         <br>
+         <!-- Jumbotron -->
+         <div class="jumbotron">
             <h1><img src="logo.png"> Get Fit!</h1>
             <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
             <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
-        </div>
-
-        <!-- Example row of columns -->
-        <div class="row">
+         </div>
+         <!-- Example row of columns -->
+         <div class="row">
             <div class="col-lg-4">
-                <h2><span class="glyphicon glyphicon-fire"></span> Exercise</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                <p><a class="btn btn-info" href="exercise.php" role="button">View details &raquo;</a></p>
+               <h2><span class="glyphicon glyphicon-fire"></span> Exercise</h2>
+               <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+               <p><a class="btn btn-info" href="exercise.php" role="button">View details &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2><span class="glyphicon glyphicon-cutlery"></span> Food</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
+               <h2><span class="glyphicon glyphicon-cutlery"></span> Food</h2>
+               <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+               <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2><span class="glyphicon glyphicon-signal"></span> Progress</h2>
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
+               <h2><span class="glyphicon glyphicon-signal"></span> Progress</h2>
+               <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+               <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
             </div>
-        </div>
-
-        <!-- Site footer -->
-        <footer class="footer text-center">
+         </div>
+         <!-- Site footer -->
+         <footer class="footer text-center">
             <p>&copy; FITrack 2015</p>
-        </footer>
-
-    </div>
-    <!-- /container -->
-
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</body>
-
+         </footer>
+      </div>
+      <!-- /container -->
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+   </body>
 </html>
